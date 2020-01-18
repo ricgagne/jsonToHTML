4ad7f388192401b7f65c537af73e0d1b34f56c94
ricgagne <58403490+ricgagne@users.noreply.github.com>
2020-01-17 22:20:11 -0500

    importToHTMLBlock OK

--- jsonToHTML.js
+++ jsonToHTML.js
@@ -1,23 +1,31 @@
+/***
+ * This function map and append a non nested json string to a html block.
+ * A new html block is created and then appended to the the last block of the list.
+ * You can use this function for post list or user list...
+ * 
+ */
 function appendToHTMLBlock(jsonStr, block) {
- var jsonObj = JSON.parse(jsonStr);
-
- $.each(jsonObj, function(key, value){
-    //var new_block = $('[data-' + block + ']').last().clone();
+  var jsonObj = JSON.parse(jsonStr);
+  // Loop through each JSON object
+  $.each(jsonObj, function(key, value){
+    // Create a new HTML block to append
     var new_block = $('[' + block + ']').last().clone();
-    
+    // Loop through each key, value pair for a JSON object
     $.each(value, function(key, value){
-        
-        $( new_block ).find('[data-' + key + ']').text(value);
-        console.log(key, value);
-
-    });
-    
+      // Map value to HTML data-key  
+      $( new_block ).find('[data-' + key + ']').text(value);
+      // console.log(key, value);
+    }); // each value
+    // append new HTML block
     new_block.insertAfter($('[' + block + ']').last());
     
-     })
-}
+  }) // each json
+} // appendToHTMLBlock
 
-function importToHTMLBlock() { }
+function importToHTMLBlock(jsonStr, block) {
+  appendToHTMLBlock(jsonStr, block);
+  $('[' + block + ']').first().remove();
+ } // importToHTMLBlock
 
 function appendToHTMLBlockBykeys() { }
 function importToHTMLBlockBykeys() { }
